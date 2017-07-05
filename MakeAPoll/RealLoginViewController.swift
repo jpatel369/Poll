1b133cb15d88c297bd5450fcc83a9dcc1b72738f
jpatel369 <jayapatel369@gmail.com>
2017-07-05 14:58:41 -0700

    First commit (Project Copied)

---
+++ MakeAPoll/RealLoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RealLoginViewController.swift
+//  MakeAPoll
+//
+//  Created by Jay Patel on 7/5/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+
+class RealLoginViewController: UIViewController{
+    @IBOutlet weak var loginButton: UIButton!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            //Jay changed this on his branch
+            
+            //Jay changed second time
+            
+            //Jay changed third time
+            
+            //Master change
+            
+            //another master change
+            
+            //Master change 3
+            
+            //Change 4
+            
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+        
+    @IBOutlet weak var loginButtonTapped: UIButton!
+        override func didReceiveMemoryWarning() {
+            
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+}
