70ed98baf313c1f0acc0d1c49aacda14a1022bb8
jpatel369 <jayapatel369@gmail.com>
2017-07-05 16:56:26 -0700

    game night commit

--- MakeAPoll/RealLoginViewController.swift
+++ MakeAPoll/RealLoginViewController.swift
@@ -22,13 +22,34 @@ class RealLoginViewController: UIViewController{
             //secondary branch change
             
             // Do any additional setup after loading the view, typically from a nib.
+            
+            UIApplication.shared.statusBarStyle = .lightContent
         }
         
     @IBOutlet weak var loginButtonTapped: UIButton!
         override func didReceiveMemoryWarning() {
             
+            guard let authUI = FUIAuth.defaultAuthUI()
+                else { return }
+            
+            authUI.delegate = self
+            
+            let authViewController = authUI.authViewController()
+            present(authViewController, animated: true)
+            
             super.didReceiveMemoryWarning()
             // Dispose of any resources that can be recreated.
         }
         
+}
+
+extension RealLoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
+        print("handle user signup / login")
+    }
 }
