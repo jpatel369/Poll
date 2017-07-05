a394f01913ec5034421c71c3d5b271d96ec4862e
jpatel369 <jayapatel369@gmail.com>
2017-07-05 15:01:46 -0700

    sendary branch change

--- MakeAPoll/RealLoginViewController.swift
+++ MakeAPoll/RealLoginViewController.swift
@@ -19,6 +19,8 @@ class RealLoginViewController: UIViewController{
             
             //New repo
             
+            //secondary branch change
+            
             // Do any additional setup after loading the view, typically from a nib.
         }
         
