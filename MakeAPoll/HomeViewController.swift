d13c6f795f6d19355bd2d6637de1d5b5b9f60af4
owasim1 <omar.wasim@valpo.edu>
2017-07-06 15:20:49 -0700

    MasterFix

--- MakeAPoll/HomeViewController.swift
+++ MakeAPoll/HomeViewController.swift
@@ -9,36 +9,10 @@
 import Foundation
 import UIKit
 
-<<<<<<< HEAD
 
 
 class HomeViewController: UIViewController
-=======
-class HomeViewController: UITableViewController
->>>>>>> swiftySyedBranch
 {
     
-    @IBOutlet weak var addPollbutton: UIBarButtonItem!
-    
-    @IBAction func addPollButtonTapped(_ sender: UISegmentedControl){
-        
-        
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ListPollsTableViewCell", for: indexPath)
-        
-        cell.textLabel?.text = "Yaaay polling!"
-        
-        return cell
-    }
-    
 }
-
-
 
